ea8cf758ecb8c8821648f7e715bb8123f5267a16
Palle Cogburn <palle@4thex.com>
2015-10-06 06:06:01 -0700

    Added some more bugs
    Put it in iframes, so they can all be seen from the main page.

--- Color/main.js
+++ Color/main.js
@@ -17,14 +17,30 @@
        for(index=0; index<arrows.length; index++) {
            arrows.item(index).addEventListener("mousedown", mousedown); 
        }
+
+       var input = function(event) {
+           update();  
+       };
+
+       var shows = document.querySelectorAll(".show");
+       for(index=0; index<shows.length; index++) {
+           shows.item(index).addEventListener("input", input); 
+       }
        var update = function() {
           var red = Number(document.querySelector("#red .show").innerText);    
           var green = Number(document.querySelector("#green .show").innerText);    
           var blue = Number(document.querySelector("#blue .show").innerText);  
           var resultElement = document.querySelector("#result .show");
-          var color = red*256*256+green*256+blue;
+          var color = "rgb("+red+", "+green+", "+blue+")";
           resultElement.innerText = color;
-          resultElement.style.backgroundColor = "#"+color.toString(16);  
+          resultElement.style.backgroundColor = color;
+          var hue = red+green+blue;
+          if(hue>400) {
+              resultElement.style.color = "black";
+          } else {
+              resultElement.style.color = "white";
+          }
        };
+       update();
     });
 }());
---
+++ Dynamic/main.js
@@ -0,0 +1,5 @@
+var divs = document.querySelectorAll("div");
+var index;
+for(index=0; index<divs.length; index++) {
+    divs.item(index).style.color = "blue";
+}
