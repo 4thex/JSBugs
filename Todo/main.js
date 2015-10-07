37f5c7d1b5286bc7d16c6c10c392ffcab3955773
Palle Cogburn <palle@4thex.com>
2015-10-07 06:17:42 -0700

    Made Todo functional
    Need to find a way to not clear the form on enter key. Perhaps this can
    be the bug?

--- Todo/main.js
+++ Todo/main.js
@@ -1,5 +1,36 @@
 (function(){
     window.addEventListener("load", function() {
-        
+        var add = document.querySelector(".add");
+        var remove = document.querySelector(".remove");
+        var addHandler = function(event) {
+           var form = document.querySelector("form");
+           var clone = form.cloneNode(true);
+           var input = clone.querySelector(".input");
+           input.firstElementChild.value = "";
+           input.style.display = "block";
+           add.style.display = "none";
+           add = clone.querySelector(".add");
+           add.style.display = "block";
+           add.addEventListener("click", addHandler);
+           remove = clone.querySelector(".remove");
+           remove.style.display = "block";
+           remove.addEventListener("click", removeHandler);
+           input = clone.querySelector(".input input");
+           input.addEventListener("keydown", function(event) {
+              if(event.keyIdentifier === "Enter") {
+                  event.preventDefault();
+              } 
+           });
+           form.parentElement.appendChild(clone); 
+        };
+        var removeHandler = function(event) {
+//            var parent = event.target.parentElement;
+//            parent.removeChild(event.target.previousElementSibling);
+//            parent.removeChild(event.target);
+           event.target.previousElementSibling.style.display = "none";
+           event.target.style.display = "none";
+        };
+        add.addEventListener("click", addHandler);
+        remove.addEventListener("click", removeHandler);        
     });
 }());
